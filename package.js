c1d701d73c8bf9363c9f11265f3d175403ab6093
T.J.T. S <T.J.T. S>
2016-09-03 11:55:39 +0200

    Update all identation to 1 tab (length 2)

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-    name: 'fullhdpixel:authenticator',
-    summary: 'Google authenticator methods for quick integration',
-    version: '0.1.2',
-    git: 'https://github.com/fullhdpixel/Google-Authenticator'
+  name: 'fullhdpixel:authenticator',
+  summary: 'Google authenticator methods for quick integration',
+  version: '0.1.2',
+  git: 'https://github.com/fullhdpixel/Google-Authenticator'
 });
 
 Package.onUse(function(api) {
-    Npm.depends({
-        'notp': '2.0.3',
-        'thirty-two': '0.0.2'
-    });
+  Npm.depends({
+    'notp': '2.0.3',
+    'thirty-two': '0.0.2'
+  });
 
-    api.addFiles(['server/authenticator.js'], 'server');
-    api.export('Authenticator', 'server');
+  api.addFiles(['server/authenticator.js'], 'server');
+  api.export('Authenticator', 'server');
 });
--- server/authenticator.js
+++ server/authenticator.js
@@ -5,22 +5,22 @@ Authenticator = {};
  * @return result object containing both a Google Authentication Code (result.key) and a qr code image of the key (result.uri)
  **/
 Authenticator.getAuthCode = function(label, issuer) {
-    var label = encodeURIComponent(label) || '';
-    var issuer = encodeURIComponent(issuer) || '';
-    var base32 = Npm.require('thirty-two');
+  var label = encodeURIComponent(label) || '';
+  var issuer = encodeURIComponent(issuer) || '';
+  var base32 = Npm.require('thirty-two');
 
-    var secret = Authenticator.createSecret();
-    var encoded = base32.encode(secret);
+  var secret = Authenticator.createSecret();
+  var encoded = base32.encode(secret);
 
-    var encodedForGoogle = encoded.toString().replace(/=/g, '');
+  var encodedForGoogle = encoded.toString().replace(/=/g, '');
 
-    var uri = 'otpauth://totp/' + label + '?secret=' + encodedForGoogle + '&issuer=' + issuer;
+  var uri = 'otpauth://totp/' + label + '?secret=' + encodedForGoogle + '&issuer=' + issuer;
 
-    var result = {
-        'uri': uri,
-        'key': secret
-    };
-    return result;
+  var result = {
+    'uri': uri,
+    'key': secret
+  };
+  return result;
 }
 
 /**
@@ -29,26 +29,26 @@ Authenticator.getAuthCode = function(label, issuer) {
  * @return Boolean, whether the code is verified or not
  **/
 Authenticator.verifyAuthCode = function(token, key) {
-    var otp = Npm.require('notp');
+  var otp = Npm.require('notp');
 
-    var verified = otp.totp.verify(token, key);
+  var verified = otp.totp.verify(token, key);
 
-    if (!verified) {
-        throw new Meteor.Error(406, 'Security code is invalid');
-    }
-    return verified;
+  if (!verified) {
+    throw new Meteor.Error(406, 'Security code is invalid');
+  }
+  return verified;
 }
 
 /**
  * @return String used for authentication code
  **/
 Authenticator.createSecret = function() {
-    var text = "";
-    var secretChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var text = "";
+  var secretChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (var i = 0; i < 20; i++) {
-        text += secretChars.charAt(Math.floor(Math.random() * secretChars.length));
-    }
+  for (var i = 0; i < 20; i++) {
+    text += secretChars.charAt(Math.floor(Math.random() * secretChars.length));
+  }
 
-    return text;
+  return text;
 }
